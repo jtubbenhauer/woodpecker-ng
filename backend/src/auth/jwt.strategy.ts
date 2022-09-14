475b828dfdc8981239e21f52e445ead8882bdaeb
Jack <jack@nimbusvapour.com.au>
2022-09-14 22:16:09 +1000

    Added reference to tutorial

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-jwt';
 
+//https://medium.com/@bruceguenkam/how-to-create-authentication-system-with-jwt-using-nestjs-and-prisma-e803d899a7a7
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
