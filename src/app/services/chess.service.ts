70540796bdb1121b68bf171683de1667827224c0
jtubbenhauer <jack@nimbusvapour.com.au>
2022-09-25 15:41:35 +1000

    Added howler.js

--- src/app/services/chess.service.ts
+++ src/app/services/chess.service.ts
@@ -1,13 +1,14 @@
-import {Injectable} from '@angular/core';
-import {Chessground} from 'chessground';
-import {Chess, Square, SQUARES} from 'chess.js';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Puzzle} from '../models/puzzle';
-import {BehaviorSubject, Subject} from 'rxjs';
-import {Api} from 'chessground/api';
-import {Color, Key} from 'chessground/types';
-import {envPrivate as env} from '../../environments/env-private';
-import {AngularFirestore} from '@angular/fire/compat/firestore';
+import { Injectable } from '@angular/core';
+import { Chessground } from 'chessground';
+import { Chess, Square, SQUARES } from 'chess.js';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Puzzle } from '../models/puzzle';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { Api } from 'chessground/api';
+import { Color, Key } from 'chessground/types';
+import { envPrivate as env } from '../../environments/env-private';
+import boardSvgs from '../utils/svg';
+import { moveSound, checkSound, captureSound } from '../utils/sounds';
 
 @Injectable({
   providedIn: 'root',
@@ -28,24 +29,7 @@ export class ChessService {
     'X-RapidAPI-Host': env.chessApiHost,
   });
 
-  svgs = {
-    right: `<g transform="translate(60 2)" >
-            <svg id="Layer_1" enable-background="new 0 0 512 512" height="40" viewBox="0 0 512 512" width="40"
-                xmlns="http://www.w3.org/2000/svg">
-                <path clip-rule="evenodd" d="m256 0c-141.2 0-256 114.8-256 256s114.8 256 256 256 256-114.8 256-256-114.8-256-256-256z"
-                fill="#4bae4f" fill-rule="evenodd"/>
-                <path
-                  d="m206.7 373.1c-32.7-32.7-65.2-65.7-98-98.4-3.6-3.6-3.6-9.6 0-13.2l37.7-37.7c3.6-3.6 9.6-3.6 13.2 0l53.9 53.9 138.6-138.7c3.7-3.6 9.6-3.6 13.3 0l37.8 37.8c3.7 3.7 3.7 9.6 0 13.2l-183.3 183.1c-3.6 3.7-9.5 3.7-13.2 0z"
-                fill="#fff"/>
-                </svg>  
-            </g>`,
-    wrong: `<g transform="translate(60 2)">
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="40" height="40" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path xmlns="http://www.w3.org/2000/svg" d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0" fill="#f44336" data-original="#f44336" class=""/><path xmlns="http://www.w3.org/2000/svg" d="m350.273438 320.105469c8.339843 8.34375 8.339843 21.824219 0 30.167969-4.160157 4.160156-9.621094 6.25-15.085938 6.25-5.460938 0-10.921875-2.089844-15.082031-6.25l-64.105469-64.109376-64.105469 64.109376c-4.160156 4.160156-9.621093 6.25-15.082031 6.25-5.464844 0-10.925781-2.089844-15.085938-6.25-8.339843-8.34375-8.339843-21.824219 0-30.167969l64.109376-64.105469-64.109376-64.105469c-8.339843-8.34375-8.339843-21.824219 0-30.167969 8.34375-8.339843 21.824219-8.339843 30.167969 0l64.105469 64.109376 64.105469-64.109376c8.34375-8.339843 21.824219-8.339843 30.167969 0 8.339843 8.34375 8.339843 21.824219 0 30.167969l-64.109376 64.105469zm0 0" fill="#fafafa" data-original="#fafafa" class=""/></g></svg>
-            </g>`,
-  };
-
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   public resetPuzzle() {
     this.puzzleComplete$.next(false);
@@ -63,7 +47,7 @@ export class ChessService {
   public backOne() {
     this.chess.undo();
     this.cg.set({
-      movable: {dests: this.getLegalMoves(), color: this.toColour()},
+      movable: { dests: this.getLegalMoves(), color: this.toColour() },
       turnColor: this.toColour(),
       fen: this.chess.fen(),
       check: this.chess.isCheck(),
@@ -81,7 +65,7 @@ export class ChessService {
     this.http
       .get<any>(env.chessApiUrl, {
         headers: this.apiHeaders,
-        params: {themes: '["promotion"]', count: '1'},
+        params: { themes: '["promotion"]', count: '1' },
       })
       .subscribe((next) => {
         this.initChessground(next.puzzles[0], el);
@@ -110,15 +94,15 @@ export class ChessService {
         },
         dests: this.getLegalMoves(),
       },
-      draggable: {showGhost: false},
+      draggable: { showGhost: false },
       events: {
         move: (orig, dest, capturedPiece) => {
           if (this.chess.inCheck()) {
-            this.playAudio('check');
+            checkSound.play();
           } else if (capturedPiece) {
-            this.playAudio('capture');
+            captureSound.play();
           } else {
-            this.playAudio('move');
+            moveSound.play();
           }
         },
       },
@@ -148,7 +132,7 @@ export class ChessService {
   }
 
   private makeMove(from: Key, to: Key) {
-    this.chess.move({from: from, to: to});
+    this.chess.move({ from: from, to: to });
     // console.log(this.chess.move({ from: from, to: to }.flag));
     this.cg.set({
       turnColor: this.toColour(),
@@ -172,12 +156,11 @@ export class ChessService {
         this.onRightMove(orig, dest);
       } else {
         this.onWrongMove(orig, dest);
-
       }
     } else {
       // If final move
       if (this.isCorrect(orig, dest)) {
-        this.cg.setAutoShapes([{orig: dest, customSvg: this.svgs.right}]);
+        this.cg.setAutoShapes([{ orig: dest, customSvg: boardSvgs.right }]);
         this.makeMove(orig, dest);
         this.cg.stop();
         this.puzzleComplete$.next(true);
@@ -192,18 +175,18 @@ export class ChessService {
     this.currentMove++;
     this.makeMove(orig, dest);
     // Make computers next move after delay
-    let {from, to} = this.convertSingleMove(this.moves[this.currentMove]);
+    let { from, to } = this.convertSingleMove(this.moves[this.currentMove]);
     this.makeMove(from as Key, to as Key);
-    this.cg.setAutoShapes([{orig: dest, customSvg: this.svgs.right}]);
+    this.cg.setAutoShapes([{ orig: dest, customSvg: boardSvgs.right }]);
     this.currentMove++;
   }
 
   private onWrongMove(orig: Key, dest: Key) {
     this.puzzleFailed$.next(true);
     this.lastMoveCorrect$.next(false);
-    this.chess.move({from: orig, to: dest});
-    this.cg.set({check: this.chess.isCheck()});
-    this.cg.setAutoShapes([{orig: dest, customSvg: this.svgs.wrong}]);
+    this.chess.move({ from: orig, to: dest });
+    this.cg.set({ check: this.chess.isCheck() });
+    this.cg.setAutoShapes([{ orig: dest, customSvg: boardSvgs.wrong }]);
     this.cg.stop();
   }
 
@@ -247,7 +230,7 @@ export class ChessService {
   private getLegalMoves(): Map<Key, Key[]> {
     const legalMoves = new Map();
     SQUARES.forEach((square) => {
-      const moves = this.chess.moves({square: square, verbose: true});
+      const moves = this.chess.moves({ square: square, verbose: true });
       if (moves.length) {
         legalMoves.set(
           square,
