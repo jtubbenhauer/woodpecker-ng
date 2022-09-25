09880037c58cf5d9810dda7cca53b2d8b2009f84
jtubbenhauer <jack@nimbusvapour.com.au>
2022-09-25 16:22:32 +1000

    Chess util functions refactoring

--- src/app/services/chess.service.ts
+++ src/app/services/chess.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Chessground } from 'chessground';
-import { Chess, Square, SQUARES } from 'chess.js';
+import { Chess } from 'chess.js';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Puzzle } from '../models/puzzle';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { Api } from 'chessground/api';
-import { Color, Key } from 'chessground/types';
+import { Key } from 'chessground/types';
 import { envPrivate as env } from '../../environments/env-private';
 import boardSvgs from '../utils/svg';
 import { moveSound, checkSound, captureSound } from '../utils/sounds';
+import {
+  convertSingleMove,
+  getLegalMoves,
+  getOrientation,
+  toColour,
+} from '../utils/chess';
 
 @Injectable({
   providedIn: 'root',
@@ -23,13 +29,55 @@ export class ChessService {
   lastMoveCorrect$ = new BehaviorSubject(true);
   puzzleComplete$ = new Subject<boolean>();
   puzzleFailed$ = new Subject<boolean>();
-  audio = new Audio();
   apiHeaders = new HttpHeaders({
     'X-RapidAPI-Key': env.chessApiKey,
     'X-RapidAPI-Host': env.chessApiHost,
   });
 
   constructor(private http: HttpClient) {}
+
+  public initChessground(puzzle: any, el: HTMLElement): any {
+    this.puzzleFailed$.next(false);
+    this.puzzleComplete$.next(false);
+    this.currentMove = 0;
+    this.puzzle = puzzle;
+    this.chess = new Chess(puzzle.fen);
+    this.cg = Chessground(el, {
+      fen: this.chess.fen(),
+      selectable: {
+        enabled: false,
+      },
+      movable: {
+        free: false,
+        color: toColour(this.chess),
+        showDests: false,
+        events: {
+          after: (orig, dest) => {
+            this.onMove(orig, dest);
+          },
+        },
+        dests: getLegalMoves(this.chess),
+      },
+      draggable: { showGhost: false },
+      turnColor: toColour(this.chess),
+      orientation: getOrientation(this.chess),
+      check: this.chess.isCheck(),
+      events: {
+        move: (orig, dest, capturedPiece) => {
+          if (capturedPiece) {
+            captureSound.play();
+          } else if (this.chess.isCheck()) {
+            checkSound.play();
+          } else {
+            moveSound.play();
+          }
+        },
+      },
+    });
+    this.moves = this.puzzle.moves as Key[];
+    this.makeFirstMove();
+    this.currentColour$.next(toColour(this.chess));
+  }
 
   public resetPuzzle() {
     this.puzzleComplete$.next(false);
@@ -47,8 +95,11 @@ export class ChessService {
   public backOne() {
     this.chess.undo();
     this.cg.set({
-      movable: { dests: this.getLegalMoves(), color: this.toColour() },
-      turnColor: this.toColour(),
+      movable: {
+        dests: getLegalMoves(this.chess),
+        color: toColour(this.chess),
+      },
+      turnColor: toColour(this.chess),
       fen: this.chess.fen(),
       check: this.chess.isCheck(),
     });
@@ -57,7 +108,7 @@ export class ChessService {
   }
 
   public getHint() {
-    let move = this.convertSingleMove(this.moves[this.currentMove]);
+    let move = convertSingleMove(this.moves[this.currentMove]);
     this.cg.selectSquare(move.from as Key);
   }
 
@@ -72,73 +123,13 @@ export class ChessService {
       });
   }
 
-  public initChessground(puzzle: any, el: HTMLElement): any {
-    this.puzzleFailed$.next(false);
-    this.puzzleComplete$.next(false);
-    this.currentMove = 0;
-    this.puzzle = puzzle;
-    this.chess = new Chess(puzzle.fen);
-    this.cg = Chessground(el, {
-      fen: this.chess.fen(),
-      selectable: {
-        enabled: false,
-      },
-      movable: {
-        free: false,
-        color: this.toColour(),
-        showDests: false,
-        events: {
-          after: (orig, dest) => {
-            this.onMove(orig, dest);
-          },
-        },
-        dests: this.getLegalMoves(),
-      },
-      draggable: { showGhost: false },
-      events: {
-        move: (orig, dest, capturedPiece) => {
-          if (this.chess.inCheck()) {
-            checkSound.play();
-          } else if (capturedPiece) {
-            captureSound.play();
-          } else {
-            moveSound.play();
-          }
-        },
-      },
-      turnColor: this.toColour(),
-      orientation: this.getOrientation(),
-      check: this.chess.isCheck(),
-    });
-    // this.moves = this.movesToArr();
-    this.moves = this.puzzle.moves as Key[];
-    this.makeFirstMove();
-    this.currentColour$.next(this.toColour());
-  }
-
-  private getPieceTypeAtOrig(orig: Key) {
-    return this.chess.get(orig as Square);
-  }
-
-  private isPromotion(orig: Key) {
-    let pieceData = this.getPieceTypeAtOrig(orig);
-    if (pieceData.type == 'p') {
-      if (pieceData.color == 'b' && orig.substring(1, 2) == '2') {
-        return true;
-      } else return pieceData.color == 'w' && orig.substring(1, 2) == '7';
-    } else {
-      return false;
-    }
-  }
-
   private makeMove(from: Key, to: Key) {
     this.chess.move({ from: from, to: to });
-    // console.log(this.chess.move({ from: from, to: to }.flag));
     this.cg.set({
-      turnColor: this.toColour(),
+      turnColor: toColour(this.chess),
       movable: {
-        color: this.toColour(),
-        dests: this.getLegalMoves(),
+        color: toColour(this.chess),
+        dests: getLegalMoves(this.chess),
       },
       check: this.chess.isCheck(),
     });
@@ -146,12 +137,10 @@ export class ChessService {
   }
 
   private onMove(orig: Key, dest: Key) {
-    console.log(this.getPieceTypeAtOrig(orig));
-
+    this.chess;
     this.cg.setAutoShapes([]);
     // If there's moves remaining in the set
     if (this.currentMove < this.moves.length - 1) {
-      // If player's move is correct
       if (this.isCorrect(orig, dest)) {
         this.onRightMove(orig, dest);
       } else {
@@ -175,10 +164,12 @@ export class ChessService {
     this.currentMove++;
     this.makeMove(orig, dest);
     // Make computers next move after delay
-    let { from, to } = this.convertSingleMove(this.moves[this.currentMove]);
-    this.makeMove(from as Key, to as Key);
-    this.cg.setAutoShapes([{ orig: dest, customSvg: boardSvgs.right }]);
-    this.currentMove++;
+    setTimeout(() => {
+      let { from, to } = convertSingleMove(this.moves[this.currentMove]);
+      this.makeMove(from as Key, to as Key);
+      this.cg.setAutoShapes([{ orig: dest, customSvg: boardSvgs.right }]);
+      this.currentMove++;
+    }, 300);
   }
 
   private onWrongMove(orig: Key, dest: Key) {
@@ -196,54 +187,9 @@ export class ChessService {
     return move === this.moves[this.currentMove];
   }
 
-  private toColour(): Color {
-    return this.chess.turn() == 'w' ? 'white' : 'black';
-  }
-
-  private getOrientation(): Color {
-    return this.chess.turn() == 'w' ? 'black' : 'white';
-  }
-
   private makeFirstMove() {
-    let move = this.convertSingleMove(this.moves[0]);
+    let move = convertSingleMove(this.moves[0]);
     this.makeMove(move.from as Key, move.to as Key);
     this.currentMove = 1;
-  }
-
-  // Convert move to object
-  private convertSingleMove(move: string) {
-    console.log(move);
-    if (move.length == 4) {
-      return {
-        from: move.substring(0, 2),
-        to: move.substring(2, 4),
-      };
-    } else {
-      return {
-        from: move.substring(0, 2),
-        to: move.substring(2, 4),
-        promotion: move.substring(4, 5),
-      };
-    }
-  }
-
-  private getLegalMoves(): Map<Key, Key[]> {
-    const legalMoves = new Map();
-    SQUARES.forEach((square) => {
-      const moves = this.chess.moves({ square: square, verbose: true });
-      if (moves.length) {
-        legalMoves.set(
-          square,
-          moves.map((move) => typeof move !== 'string' && move.to)
-        );
-      }
-    });
-    return legalMoves;
-  }
-
-  private playAudio(type: 'capture' | 'move' | 'check') {
-    this.audio.src = `../assets/${type}.mp3`;
-    this.audio.load();
-    this.audio.play();
   }
 }
---
+++ src/app/utils/chess.ts
@@ -0,0 +1,54 @@
+import { Chess, Square, SQUARES } from 'chess.js';
+import { Key } from 'chessground/types';
+
+export const toColour = (chess: Chess) => {
+  return chess.turn() == 'w' ? 'white' : 'black';
+};
+
+export const getOrientation = (chess: Chess) => {
+  return chess.turn() == 'w' ? 'black' : 'white';
+};
+
+export const convertSingleMove = (move: string) => {
+  if (move.length == 4) {
+    return {
+      from: move.substring(0, 2),
+      to: move.substring(2, 4),
+    };
+  } else {
+    return {
+      from: move.substring(0, 2),
+      to: move.substring(2, 4),
+      promotion: move.substring(4, 5),
+    };
+  }
+};
+
+export const getLegalMoves = (chess: Chess) => {
+  const legalMoves = new Map();
+  SQUARES.forEach((square) => {
+    const moves = chess.moves({ square: square, verbose: true });
+    if (moves.length) {
+      legalMoves.set(
+        square,
+        moves.map((move) => typeof move !== 'string' && move.to)
+      );
+    }
+  });
+  return legalMoves;
+};
+
+export const getPieceTypeAtOrig = (orig: Key, chess: Chess) => {
+  return chess.get(orig as Square);
+};
+
+export const isPromotion = (orig: Key, chess: Chess) => {
+  let pieceData = getPieceTypeAtOrig(orig, chess);
+  if (pieceData.type == 'p') {
+    if (pieceData.color == 'b' && orig.substring(1, 2) == '2') {
+      return true;
+    } else return pieceData.color == 'w' && orig.substring(1, 2) == '7';
+  } else {
+    return false;
+  }
+};
