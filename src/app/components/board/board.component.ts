8bc663de6f2bb3c425c1de0259083919a0efb0a7
Jack <jack@nimbusvapour.com.au>
2022-09-28 10:04:04 +1000

    Added manual board resizing

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -7,6 +7,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { ChessService } from '../../services/chess.service';
+import interact from 'interactjs';
 
 @Component({
   selector: 'app-board',
@@ -15,6 +16,8 @@ import { ChessService } from '../../services/chess.service';
 })
 export class BoardComponent implements OnInit, AfterViewInit {
   @ViewChild('chessboard') el!: ElementRef<HTMLElement>;
+  @ViewChild('chessDiv') chessDiv!: ElementRef<HTMLElement>;
+
   boardSize = '580px';
   length = 580;
   mouseDown?: number;
@@ -25,7 +28,29 @@ export class BoardComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {}
 
   ngAfterViewInit() {
-    // this.calcDimensions(this.length);
+    console.log(this.el.nativeElement);
+    interact(this.el.nativeElement).resizable({
+      edges: {
+        right: true,
+        bottom: true,
+      },
+      listeners: {
+        move: (event) => {
+          let { x, y } = event.target.dataset;
+
+          x = (parseFloat(x) || 0) + event.deltaRect.left;
+          y = (parseFloat(y) || 0) + event.deltaRect.top;
+
+          Object.assign(event.target.style, {
+            width: `${event.rect.width}px`,
+            height: `${event.rect.width}px`,
+            transform: `translate(${x}px, ${y}px)`,
+          });
+
+          Object.assign(event.target.dataset, { x, y });
+        },
+      },
+    });
   }
 
   calcDimensions(length: number) {
@@ -38,7 +63,7 @@ export class BoardComponent implements OnInit, AfterViewInit {
 
   onMouseMove(event: MouseEvent) {
     if (this.mouseDown) {
-      this.length = this.length - (event.pageX - this.mouseDown);
+      this.length = this.length + (this.mouseDown - event.pageX);
     }
     this.calcDimensions(this.length);
   }
